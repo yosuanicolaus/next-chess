9e10ed97ef11dcdb84f2d5c753969f2e7003ea8f
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-05 19:08:59 +0700

    create types.ts for schemas & validations

---
+++ src/utils/types.ts
@@ -0,0 +1,76 @@
+export interface User {
+  uid: string;
+  name: string;
+  elo: number;
+  createdAt: string;
+}
+
+export interface Message {
+  id: string;
+  text: string;
+  name: string;
+  uid: string;
+  createdAt: string;
+}
+
+export interface Chat {
+  id: string;
+  messages: Message[];
+  createdAt: string;
+}
+
+export interface Player {
+  uid: string;
+  name: string;
+  elo: number;
+  active: boolean;
+  online: boolean;
+  time: number;
+}
+
+export interface Move {
+  from: {
+    rank: number;
+    file: number;
+  };
+  to: {
+    rank: number;
+    file: number;
+  };
+  piece: string;
+  faction: "w" | "b";
+  san: string;
+  lan: string;
+  uci: string;
+  fenResult: string;
+  capture?: true;
+}
+
+export type GameState =
+  | "empty"
+  | "waiting"
+  | "pending"
+  | "ready"
+  | "playing"
+  | "ended";
+
+export type GameStatus = "normal" | "check" | "end";
+
+export interface GameModel {
+  id: string;
+  timeControl: string;
+  state: GameState;
+  status: GameStatus;
+  turn: "w" | "b";
+  fen: string;
+  board: string[][];
+  moves: Move[];
+  history: string[];
+  records: number[];
+  pgn: string;
+  pwhite: Player;
+  pblack: Player;
+  user0: User;
+  user1: User;
+  chatID: string;
+}
