91e16e9328362d3f3d8e6b35f6b91c846fecbc9e
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-24 09:44:58 +0700

    refactor and add light/dark mode to Navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { useUser } from "../../lib/contexts/auth";
@@ -8,53 +7,56 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="flex bg-zinc-700 p-5 text-neutral-100">
+    <nav className="flex bg-gray-300 p-5 text-neutral-700 dark:bg-zinc-700 dark:text-neutral-100">
       {/* for large screen */}
       <div className="hidden flex-grow items-center gap-5 sm:flex">
-        <Link href="/">
-          <a className="text-2xl transition hover:text-green-300">LogiChess</a>
-        </Link>
-        <Link href="/game">
-          <a className="transition hover:text-green-300">Game</a>
-        </Link>
-        <Link href={`user/${user.name}`}>
-          <a className="ml-auto transition-colors hover:text-green-300">
-            {user.name}
-          </a>
-        </Link>
+        <NavLink href="/" text="LogiChess" className="text-2xl" />
+        <NavLink href="/game" text="Game" />
+        <NavLink
+          href={`/user/${user.name}`}
+          text={user.name}
+          className="ml-auto"
+        />
       </div>
 
       {/* for small screen */}
       <div className="flex flex-grow flex-col sm:hidden">
         <div className="flex">
-          <Link href="/">
-            <a className="text-2xl transition hover:text-green-300">
-              LogiChess
-            </a>
-          </Link>
+          <NavLink href="/" text="LogiChess" className="text-2xl" />
           <button
-            className="ml-auto rounded-lg"
+            className="ml-auto rounded-lg text-2xl hover:text-cyan-700 dark:hover:text-green-300"
             onClick={() => setOpen((val) => !val)}
           >
-            <Image
-              src={"/assets/navbar.svg"}
-              alt="menu"
-              width={32}
-              height={32}
-              className="hover:bg-zinc-600"
-            />
+            &#9776;
           </button>
         </div>
         {/* when button is click, show these items */}
         <div className={open ? "flex flex-col gap-1" : "hidden"}>
-          <Link href={"/game"}>
-            <a className="hover:text-green-300">Game</a>
-          </Link>
-          <Link href={`user/${user.name}`}>
-            <a className="hover:text-green-300">{user.name}</a>
-          </Link>
+          <NavLink href="/game" text="Game" className="mt-3" />
+          <NavLink href={`/user/${user.name}`} text={user.name} />
         </div>
       </div>
     </nav>
+  );
+}
+
+type NavLinkType = {
+  href: string;
+  text: string;
+  className?: string;
+};
+
+function NavLink({ href, text, className }: NavLinkType) {
+  return (
+    <Link href={href}>
+      <a
+        className={
+          "transition hover:text-cyan-700 dark:hover:text-green-300 " +
+          className
+        }
+      >
+        {text}
+      </a>
+    </Link>
   );
 }
