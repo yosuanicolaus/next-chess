24157e4e822b0c33afb578826ba625b946bf1ccb
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-24 10:35:26 +0700

    redesign Background color & opacity

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export function Background() {
   return (
-    <div className="absolute top-0 left-0 -z-10 grid min-h-screen w-screen place-content-center bg-gradient-to-t from-slate-200 to-slate-50 dark:from-slate-600 dark:to-slate-800">
+    <div className="absolute top-0 left-0 -z-10 grid min-h-screen w-screen place-content-center bg-gradient-to-t from-slate-200 to-slate-50 dark:from-neutral-600 dark:to-neutral-800">
       <div className="block dark:hidden">
         <Image
           src={"/assets/nc-bg-light.svg"}
           alt={"logichess"}
           width={400}
           height={300}
-          className="opacity-10"
+          className="opacity-20"
         />
       </div>
       <div className="hidden dark:block">
@@ -18,7 +18,7 @@ export function Background() {
           alt={"logichess"}
           width={400}
           height={300}
-          className="opacity-10"
+          className="opacity-20"
         />
       </div>
     </div>
