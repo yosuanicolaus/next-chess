8bcc5a6e8ef0c602d466abb18c69d8dd3520714a
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-24 10:01:37 +0700

    redesign Footer with light/dark mode

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
-    <div className="bg-zinc-700 text-center text-neutral-100">
-      <div>&copy; copyright Yosua Nicolaus 2022</div>
+    <div className="bg-gray-300 p-1 text-center text-neutral-700 text-opacity-50 transition hover:text-cyan-600 dark:bg-zinc-800 dark:text-neutral-100/50">
+      <a
+        href="https://github.com/yosuanicolaus/next-chess"
+        target="_blank"
+        rel="noreferrer"
+      >
+        &copy; yosuanicolaus 2022
+      </a>
     </div>
   );
 }
