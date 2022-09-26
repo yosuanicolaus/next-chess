025feee409693f36c1763b0183c9119d4edd476c
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-26 11:13:29 +0700

    fix and improve leaveGame db function
    use gameID instead of game object
    remove by setting null instead of undefined
    deny permission if uid is not user0's AND user1's
    (instead of OR, which is wrong)
    add console log for every denied returns.

--- src/lib/db/game/leave.ts
+++ src/lib/db/game/leave.ts
@@ -1,29 +1,34 @@
-import { set } from "firebase/database";
+import { get, update } from "firebase/database";
 import { gamesRef } from "../../firebase";
 import { Game, TransformGame } from "../../types";
 import { User } from "../../types";
 
-export async function leaveGame(game: Game, user: User) {
-  const newGame: TransformGame = game;
+export async function leaveGame(gameID: string, user: User) {
+  const gameSnapshot = await get(gamesRef(gameID));
+  const game: Game = gameSnapshot.val();
+  const newGame: TransformGame = Object.assign({}, game);
 
   if (game.state === "empty") {
+    console.log("Denied: game is already empty");
     return;
   } else if (game.state === "waiting") {
     if (game.user0.uid === user.uid) {
       newGame.state = "empty";
-      newGame.user0 = undefined;
+      newGame.user0 = null;
     } else {
+      console.log("Denied: user.uid !== game.user0.uid");
       return;
     }
-  } else if (user.uid !== game.user0.uid || user.uid !== game.user1.uid) {
+  } else if (user.uid !== game.user0.uid && user.uid !== game.user1.uid) {
+    console.log("Denied: user.uid !== game.user(0/1).uid");
     return;
   } else if (game.state === "pending" || game.state === "ready") {
     newGame.state = "waiting";
     if (user.uid === game.user0.uid) {
       newGame.user0 = game.user1;
-      newGame.user1 = undefined;
+      newGame.user1 = null;
     } else if (user.uid === game.user1.uid) {
-      newGame.user1 = undefined;
+      newGame.user1 = null;
     }
   } else if (game.state === "playing" || game.state === "ended") {
     if (user.uid === game.pwhite.uid && newGame.pwhite) {
@@ -32,5 +37,5 @@ export async function leaveGame(game: Game, user: User) {
       newGame.pblack.online = false;
     }
   }
-  set(gamesRef(game.id), newGame);
+  update(gamesRef(game.id), newGame);
 }
