ee3abf5f622980e72f6c492b3404fb2427ef93e9
Yosua Nicolaus <cryodawn257@gmail.com>
2022-10-05 12:45:19 +0700

    import and use 3 board layer

--- src/components/game/Board/index.tsx
+++ src/components/game/Board/index.tsx
@@ -1,15 +1,20 @@
 import { BoardProvider } from "../../../lib/contexts/board";
 import { useSize } from "../../../lib/contexts/size";
+import { BackPanels } from "./BackPanels";
+import { FrontPanels } from "./FrontPanels";
+import { Pieces } from "./Pieces";
 
 export function Board() {
   const { size } = useSize();
   return (
     <main
-      className={`relative bg-[url('/boards/blue.svg')] bg-no-repeat`}
+      className="relative bg-[url('/boards/blue.svg')] bg-no-repeat"
       style={{ width: size, height: size }}
     >
       <BoardProvider>
-        <div>test</div>
+        <BackPanels />
+        <Pieces />
+        <FrontPanels />
       </BoardProvider>
     </main>
   );
