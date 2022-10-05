4e47351e2e119b8744b628e6c1fb67b046db4d15
Yosua Nicolaus <cryodawn257@gmail.com>
2022-10-05 13:39:40 +0700

    fix & restyle playerinfo online & active status

--- src/components/game/InfoWidget/PlayInfo/PlayerInfo.tsx
+++ src/components/game/InfoWidget/PlayInfo/PlayerInfo.tsx
@@ -7,23 +7,25 @@ export function PlayerInfo({ player }: { player: Player }) {
     player.uid === game.pwhite.uid
       ? "bg-gradient-to-br from-white dark:from-slate-600"
       : "bg-gradient-to-br from-slate-400 dark:from-slate-800";
-  const onlineStatus = player.active ? (
-    <span>online</span>
-  ) : (
-    <span className="text-neutral-500">offline</span>
+
+  const OnlineStatus = () => (
+    <>
+      {player.online && <span>online</span>}
+      {!player.online && <span className="opacity-50">offline</span>}
+    </>
   );
-  const turnStatus =
-    player.uid === game.pwhite.uid && game.turn === "w" ? (
-      <span className="text-green-600">active</span>
-    ) : (
-      <span className="text-neutral-300">waiting</span>
-    );
+  const TurnStatus = () => (
+    <>
+      {player.active && <span className="text-green-600">active</span>}
+      {!player.active && <span className="opacity-50">waiting</span>}
+    </>
+  );
 
   return (
     <div className={playerTheme + " p-1"}>
       <div className="font-medium">{player.name}</div>
       <div className="text-sm italic">
-        {onlineStatus}, {turnStatus}
+        <OnlineStatus />, <TurnStatus />
       </div>
     </div>
   );
