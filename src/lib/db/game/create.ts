d1c270f926edd692247a0f518869f487bfa6dfdb
Yosua Nicolaus <cryodawn257@gmail.com>
2022-10-12 14:21:03 +0700

    create createNewGameWithBot game db fn
    immediately creates a GameComplete object, because
    having a pre-game state is unnecessary here
    
    accepts user, botAlgorithm params, and optional timeControl params

--- src/lib/db/game/create.ts
+++ src/lib/db/game/create.ts
@@ -1,7 +1,10 @@
 import { set } from "firebase/database";
+import { BotAlgorithm } from "logichess-bots";
 import { gamesRef } from "../../firebase";
-import { Game } from "../../types";
+import { Game, GameComplete, User } from "../../types";
 import { generateID } from "../../utils";
+import { createNewBotUser, createPlayer } from "../user";
+import { defaultBoard, defaultFen, defaultMoves } from "./_data";
 
 export async function createNewGame(timeControl = "10+5") {
   const id = generateID(10);
@@ -13,5 +16,36 @@ export async function createNewGame(timeControl = "10+5") {
     timeControl,
   };
   await set(gamesRef(id), game);
+  return game;
+}
+
+export async function createNewGameWithBot(
+  user: User,
+  botAlgorithm: BotAlgorithm,
+  timeControl = "10+5"
+) {
+  const botUser = createNewBotUser(botAlgorithm);
+  const flipped = Math.random() < 0.5;
+  const pwhiteUser = flipped ? user : botUser;
+  const pblackUser = flipped ? botUser : user;
+
+  const game: GameComplete = {
+    id: generateID(10),
+    chatID: generateID(12),
+    timeControl,
+    state: "playing",
+    status: "normal",
+    difference: 0,
+    turn: "w",
+    fen: defaultFen,
+    board: defaultBoard,
+    moves: defaultMoves,
+    pgn: "",
+    user0: user,
+    user1: botUser,
+    pwhite: createPlayer(pwhiteUser, timeControl, true),
+    pblack: createPlayer(pblackUser, timeControl),
+  };
+  await set(gamesRef(game.id), game);
   return game;
 }
