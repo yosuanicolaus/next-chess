7bc16c7b22d5b90864a56ea12bff2784c76b67fb
Yosua Nicolaus <cryodawn257@gmail.com>
2022-10-12 11:49:13 +0700

    add logichess-bots & create play move function

--- src/lib/chess.ts
+++ src/lib/chess.ts
@@ -1,6 +1,21 @@
 import { Chess } from "logichess";
+import { Bot, BotAlgorithm } from "logichess-bots";
+import { updateChessData } from "./db/game";
+import { GameComplete, Move } from "./types";
 
 export function createChessData(fen: string) {
   const chess = new Chess(fen);
   return chess.data;
+}
+
+export function playChessMove(game: GameComplete, move: Move) {
+  const chess = new Chess(move.fenResult);
+  updateChessData(game, chess.data, move);
+}
+
+export function playBotMove(game: GameComplete, algorithm: BotAlgorithm) {
+  const chess = new Chess(game.fen);
+  const bot = new Bot(chess, algorithm);
+  chess.play(bot.move);
+  updateChessData(game, chess.data, bot.move);
 }
